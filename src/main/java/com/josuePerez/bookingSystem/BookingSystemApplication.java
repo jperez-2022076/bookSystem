c93eec6f7f442c517eae338d3e34b1b38071db72
jperez-2022076 <jperez-2022076@kinal.edu.gt>
2024-09-10 08:27:03 -0600

    Primera fase

---
+++ src/main/java/com/josuePerez/bookingSystem/BookingSystemApplication.java
@@ -0,0 +1,14 @@
+package com.josuePerez.bookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BookingSystemApplication.class, args);
+	}
+
+}
